37b382047ce85a9ffa7c21c2b90bbee3eaf5dbb5
Anna Marie Cooper and Bryce Frazier <anna@>
2018-02-12 11:59:07 -0800

    app working except for logic flaw with non triangle.

--- app.rb
+++ app.rb
@@ -1,10 +1,18 @@
 require('sinatra')
 require('sinatra/reloader')
-# require('pry')
-# require('rscpec')
-# require('triangle')
+also_reload('lib/**/*.rb')
+require('./lib/triangle')
 
-get ('/triangle') do
-  @description = "This application will determine what kind of triangle based on user input"
-  erb(:input)
-end
+  get ('/') do
+    @description = "This application will determine what kind of triangle based on user input"
+    erb(:input)
+  end
+
+  get('/output') do
+    @side1 = params.fetch("side1")
+    @side2 = params.fetch("side2")
+    @side3 = params.fetch("side3")
+    triangle = Triangle.new(@side1, @side2, @side3)
+    @string_to_display = triangle.triangle_checker
+    erb(:output)
+  end
